56230fe07023d9c7484166038872409f29d589cd
Ritik Patil <ritikpatil370@gmail.com>
2020-03-06 22:13:17 +0530

    NC

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 4000;
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.listen(PORT, () => {
+  console.log("app running on PORT : ", PORT);
+});
