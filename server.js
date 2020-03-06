0b17ce1c60532f948c5f0e31c68c11adfd2e6b2d
Ritik Patil <ritikpatil370@gmail.com>
2020-03-06 23:15:41 +0530

    nc7

--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "")));
 
-const PORT = process.env.PORT || 4000;
+// const PORT = process.env.PORT || 4000;
 
 // app.get("/", (req, res) => {
 //   res.redirect();
 // });
-app.listen(PORT, () => {
-  console.log("app running on PORT : ", PORT);
+app.listen(process.env.PORT || 3000, function() {
+  console.log(
+    "Express server listening on port %d in %s mode",
+    this.address().port,
+    app.settings.env
+  );
 });
